9f4303eda029e12dfec49ba30b643c19ed9c1ddb
Jake Butler <JakeButler.student@careerdevs.com>
2022-12-29 23:24:30 -0500

    Completed Java Inheritance example

---
+++ Inherit/src/Mouse.java
@@ -0,0 +1,18 @@
+public class Mouse {
+
+    public void leftClick() {
+        System.out.println("Left click!");
+    }
+
+    public void rightClick() {
+        System.out.println("Right click!");
+    }
+
+    public void scrollUp() {
+        System.out.println("Scrolled up.");
+    }
+
+    public void scrollDown() {
+        System.out.println("Scrolled down.");
+    }
+}
---
+++ Inherit/src/Mouse3.java
@@ -0,0 +1,5 @@
+public class Mouse3 extends Mouse{
+
+    public boolean ambidextrous = true;
+
+}
